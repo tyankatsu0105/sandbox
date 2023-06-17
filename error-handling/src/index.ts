c2277806bd3ca639ca7fe25a3efb7a160b067b17
tyankatsu0105 <frips.ryilsufupe+dev@gmail.com>
2023-06-17 14:37:58 +0900

    error

--- error-handling/src/index.ts
+++ error-handling/src/index.ts
@@ -1,2 +1,24 @@
-const hoge = "hoge";
-console.log(hoge);
+import { invalid, isValid, valid } from "./shared/error";
+
+const fn = (value: number) => {
+  if (value === 0) return valid("a" as const);
+  if (value === 1) return invalid("b" as const);
+  if (value === 2) return valid(true as const);
+  if (value === 3) return invalid([1] as const);
+  if (value === 4) return valid("" as const);
+
+  return invalid("error" as const);
+};
+
+const main = () => {
+  const result = fn(10);
+
+  if (isValid(result)) {
+    console.log(result.value);
+    return;
+  }
+
+  console.log(result.error);
+};
+
+main();
--- error-handling/src/shared/error/index.ts
+++ error-handling/src/shared/error/index.ts
@@ -15,31 +15,3 @@ export const isValid = (result: Result): result is Valid<unknown> =>
   result.type === "valid";
 export const isInvalid = (result: Result): result is Invalid<unknown> =>
   result.type === "invalid";
-
-const fn = (value: number) => {
-  if (value === 0) return valid("a" as const);
-  if (value === 1) return invalid("b" as const);
-  if (value === 2) return valid(true as const);
-  if (value === 3) return invalid([1] as const);
-  if (value === 4) return valid("" as const);
-
-  return invalid("error" as const);
-};
-
-const main = () => {
-  const result = fn(0);
-
-  if (isValid(result)) {
-    if (result.value === "a") return;
-    return;
-  }
-
-  //   if (isInvalid(result)) {
-  //     if (result.error === 'b') return;
-  //     return;
-  //   }
-
-  result.error;
-};
-
-main();
